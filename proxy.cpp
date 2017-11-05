afbf92814a28891675c58a8bae0f2ce65cef3e40
ellerybaines <ellerybaines@gmail.com>
2017-11-04 20:07:26 -0700

    formatting

--- proxy.cpp
+++ proxy.cpp
@@ -28,329 +28,273 @@ http://beej.us/guide/bgnet/output/html/multipage/clientserver.html
 #include <iostream>
 #include <vector>
 
-
 #define BACKLOG 30	 // how many pending connections queue will hold
 #define MAXDATASIZE 100 // max number of bytes we can get at once 
 
-
-void sigchld_handler(int s)
-{
+void sigchld_handler(int s){
 	// waitpid() might overwrite errno, so we save and restore it:
 	int saved_errno = errno;
-
 	while(waitpid(-1, NULL, WNOHANG) > 0);
-
 	errno = saved_errno;
 }
-void sigpipe_handler(int s){
 
+void sigpipe_handler(int s){
 }
 
-
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
+void *get_in_addr(struct sockaddr *sa){
+	// get sockaddr, IPv4 or IPv6:
 	if (sa->sa_family == AF_INET) {
 		return &(((struct sockaddr_in*)sa)->sin_addr);
 	}
-
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-
 void sendResponseToClient(std::string serverResponse, int new_fd){
-
 	int bytesToSend2 = serverResponse.length() + 1;
 	int bytesSent2 = 0;
 	char* response_tosend = new char[bytesToSend2];
-	strcpy(response_tosend, serverResponse.c_str());
-		  	
+	strcpy(response_tosend, serverResponse.c_str());  	
 	char* bufptr2 = response_tosend;
 		 	
 	while ((bytesSent2 = send(new_fd, bufptr2, bytesToSend2, 0)) > 0){
-
-			bytesToSend2 = bytesToSend2 - bytesSent2;
-			bufptr2 += bytesSent2;
-			if (bytesToSend2 <1){
-				break;	
-			}
-	delete[] response_tosend;
-			}
-			
+		bytesToSend2 = bytesToSend2 - bytesSent2;
+		bufptr2 += bytesSent2;
+		if (bytesToSend2 <1){
+			break;	
+		}
+		delete[] response_tosend;
+	}	
 }
 
 void* doRequest(void* in){
-			printf("in thread");
-			int bytesRecieved = -2;
-			int bufSpace = MAXDATASIZE-1;
-			char recieveBuffer[MAXDATASIZE];
-			std::string request_input = "";
-			char* bufptr = recieveBuffer;
-			int new_fd = *((int*) in);
+	printf("in thread");
+	int bytesRecieved = -2;
+	int bufSpace = MAXDATASIZE-1;
+	char recieveBuffer[MAXDATASIZE];
+	std::string request_input = "";
+	char* bufptr = recieveBuffer;
+	int new_fd = *((int*) in);
+		
+	while((bytesRecieved = recv(new_fd, bufptr, bufSpace, 0)) > 0){
+		for (int i = 0; i < bytesRecieved; i++){
+			request_input += bufptr[i]; 
+		}
+		memset(bufptr, 0, MAXDATASIZE-1);
+		// storing it in a string handles memory so we dont have to worry about buffer overflow.
+		// we can now reuse/overwrite on the same buffer
+	
+		if (request_input.length() > 3 && request_input.at(request_input.length()-1) == '\n' && request_input.at(request_input.length()-2) == '\r' && request_input.at(request_input.length()-3) == '\n' && request_input.at(request_input.length()-4) == '\r'){
+			break; 
+		}
+	}
+	printf("%s\n", request_input.c_str() );
 
-			
-			while((bytesRecieved = recv(new_fd, bufptr, bufSpace, 0)) > 0){
-				
-				for (int i = 0; i < bytesRecieved; i++){
-					request_input += bufptr[i]; 
-				}
-				memset(bufptr, 0, MAXDATASIZE-1);
-				// storing it in a string handles memory so we dont have to worry about buffer overflow.
-				// we can now reuse/overwrite on the same buffer
-			
-				if (request_input.length() > 3 && request_input.at(request_input.length()-1) == '\n' && request_input.at(request_input.length()-2) == '\r' && request_input.at(request_input.length()-3) == '\n' && request_input.at(request_input.length()-4) == '\r') 
-					break; 
+	char* request_in = new char[request_input.length()+1];
+	strcpy(request_in, request_input.c_str());
+	int size = 3;
+	int index = 3;
+	std::vector<char*> lines(size);
+	std::string serverResponse = "";	
+	    
+    // parsing first line of input request
+	char* request = strtok(request_in, " ");
+	
+	if (request == NULL){
+	    serverResponse += "500 error: Internal Server error\n";
+		sendResponseToClient(serverResponse, new_fd);
+		return NULL;
+	}
 
-			}
+	char* url = strtok(NULL, " ");
 
-			printf("%s\n", request_input.c_str() );
+	if (url == NULL){
+	    serverResponse += "500 error: Internal Server error\n";
+		sendResponseToClient(serverResponse, new_fd);
+		return NULL;
+	}
 
+	char* http_type = strtok(NULL, "\n");
 
-			
-			char* request_in = new char[request_input.length()+1];
-			strcpy(request_in, request_input.c_str());
-			int size = 3;
-			int index = 3;
-			std::vector<char*> lines(size);
-			std::string serverResponse = "";
-			
-		    
-		    // parsing first line of input request
-			char* request = strtok(request_in, " ");
-			
-			if (request == NULL){
-				    serverResponse += "500 error: Internal Server error\n";
-					sendResponseToClient(serverResponse, new_fd);
-					return NULL;
+	// adding parse to vector
+	lines[0] = request;
+	lines[1] = url;
+	lines[2] = http_type;
 
-			}
+	// recieves additional lines
+	char* line = strtok(NULL, "\n");
+	while(line){
+		// resize function
+		if((unsigned) index == lines.size()){
+			size = size + 1;
+			lines.resize(size);	
+		}
+		lines[index] = line;
+		line = strtok(NULL, "\n");
+		index += 1;
+	}
 
-			char* url = strtok(NULL, " ");
+	lines.pop_back(); // remove blank entry
+	index = index - 1;
 
-			if (url == NULL){
-				    serverResponse += "500 error: Internal Server error\n";
-					sendResponseToClient(serverResponse, new_fd);
-					return NULL;
-			}
+	bool isFormated = true;
+	std::string request_formatted = "";
+	std::string unformatted = lines[1];
+	
+	int start = unformatted.find( "//", 0);
 
-			char* http_type = strtok(NULL, "\n");
+	if ((unsigned)start == std::string::npos){
+		isFormated = false;
+	}
 
-			// adding parse to vector
-			lines[0] = request;
-			lines[1] = url;
-			lines[2] = http_type;
+	int relitive_start = unformatted.find( "/", start+2);
+	int port_specified = unformatted.find(":", start+2);
+	std::string host;
+	std::string path;
+	std::string port;
+	if (relitive_start != -1){
+		if (port_specified != -1){
+			port = unformatted.substr(port_specified + 1, relitive_start- (port_specified+1));
+			path = unformatted.substr(relitive_start, std::string::npos);
+			host = unformatted.substr(start+2, port_specified-(start+2));
+		}
+		else{
+			port = "80";
+			path = unformatted.substr(relitive_start, std::string::npos);
+			host = unformatted.substr(start+2, relitive_start-(start+2));
+		}
+	}
+	else{
+		if (port_specified != -1){
+			port = unformatted.substr(port_specified + 1, relitive_start- (port_specified+1));
+			path = "/";
+			host = unformatted.substr(start+2, port_specified-(start+2));
+		}
+		else{
+			port = "80";
+			path = "/";
+			host = unformatted.substr(start+2, std::string::npos);
+		}
+	}
 
-			// recieves additional lines
-			char* line = strtok(NULL, "\n");
-			while(line){
-				// resize function
-				if((unsigned) index == lines.size()){
-					size = size + 1;
-					lines.resize(size);	
-				}
-				lines[index] = line;
-				line = strtok(NULL, "\n");
-				index += 1;
-			}
-
-			lines.pop_back(); // remove blank entry
-			index = index - 1;
-
-			// DEBUG - print out of vector
-			//for (int i = 0; (unsigned) i < lines.size(); i++){
-			//	printf("%d%s%s\n", i, " : ", lines[i]);
-			//}
-
-			bool isFormated = true;
-			std::string request_formatted = "";
-			std::string unformatted = lines[1];
-			
-			int start = unformatted.find( "//", 0);
-
-			if ((unsigned)start == std::string::npos)
-				isFormated = false;
-
-			int relitive_start = unformatted.find( "/", start+2);
-			int port_specified = unformatted.find(":", start+2);
-			std::string host;
-			std::string path;
-			std::string port;
-			if (relitive_start != -1){
-
-				if (port_specified != -1){
-					port = unformatted.substr(port_specified + 1, relitive_start- (port_specified+1));
-					path = unformatted.substr(relitive_start, std::string::npos);
-					host = unformatted.substr(start+2, port_specified-(start+2));
-
-				}
-				
-				else{
-					port = "80";
-					path = unformatted.substr(relitive_start, std::string::npos);
-					host = unformatted.substr(start+2, relitive_start-(start+2));
-
-				}
-
-			}
-			else{
-				if (port_specified != -1){
-					port = unformatted.substr(port_specified + 1, relitive_start- (port_specified+1));
-					path = "/";
-					host = unformatted.substr(start+2, port_specified-(start+2));
-
-				}
-				
-				else{
-					port = "80";
-					path = "/";
-					host = unformatted.substr(start+2, std::string::npos);
-
-				}
-
-				
-			}
-
-			
-			
-			request_formatted += "GET ";
-			request_formatted += path;
-			request_formatted += " HTTP/1.0\r\n";
-			request_formatted += "Host:" + host + "\r\n";
-			request_formatted += "Connection:close \r\n";
+	request_formatted += "GET ";
+	request_formatted += path;
+	request_formatted += " HTTP/1.0\r\n";
+	request_formatted += "Host:" + host + "\r\n";
+	request_formatted += "Connection:close \r\n";
 
 
 	/*
-			int colon, colon2;
-			
-
-			// add option lines to the formatted string
-			for (int i = 3; i < index; i++){
-				unformatted = lines[i];
-				colon = unformatted.find(":", 0);
-				colon2 = unformatted.find(":", colon+1);
-				// check if no colon is present in line
-				if(colon == -1 ){
-					isFormated = false;
-				}2
-				// check if there are multiple colons in a single line
-				else if(colon2 != -1 ){
-					isFormated = false;
-
-				}
-				std::string header = lines[i];
-				if (header.find("connection", 0) == std::string::npos && header.find("Connection", 0) == std::string::npos){ //we always want connection:close regardless of user input
-					request_formatted += lines[i];
-					request_formatted += "\r\n";
-					
-				}
-			}
-			
-			// error checks and server response
-			*/
-			// request check
-
-			request_formatted += "\r\n";
-			printf("%s\n",request_formatted.c_str() );
-
-			if (strcmp(lines[0], "GET") != 0){
-				serverResponse += "501 Error: Request type not supported\n";
-			}
-			// options formatting check
-			else if (!isFormated){
-				serverResponse += "500 Error: Internal Server error\n";
-			}
-			// proxy response piping
-			else{
-				int request_fd;
-				struct addrinfo hints1, *servinfo1, *p1;
-				int rv1;
-
-				memset(&hints1, 0, sizeof hints1);
-				hints1.ai_family = AF_UNSPEC;
-				hints1.ai_socktype = SOCK_STREAM;
-				hints1.ai_flags = AI_PASSIVE; // use my IP
-
-				
-				if ((rv1 = getaddrinfo(host.c_str(), port.c_str(), &hints1, &servinfo1)) != 0) {
-					serverResponse += "500 error: Internal Server error\n";
-					sendResponseToClient(serverResponse, new_fd);
-					return NULL;
-				}
-
-				// loop through all the results and connect to the first we can
-				for(p1 = servinfo1; p1 != NULL; p1 = p1->ai_next) {
-					if ((request_fd = socket(p1->ai_family, p1->ai_socktype,
-							p1->ai_protocol)) == -1) {
-
-						return NULL;
-					}
-
-					if (connect(request_fd, p1->ai_addr, p1->ai_addrlen) == -1) {
-						close(request_fd);
-						return NULL;
-					}
-
-					break;
-				}
-
-				if (p1 == NULL) {
-					serverResponse += "500 error: Internal Server error\n";
-					sendResponseToClient(serverResponse, new_fd);
-					return NULL; // will modularize in next version
-				}
-
-				freeaddrinfo(servinfo1); // all done with this structure
-
-
-			
-				int bytesToSend1 = request_formatted.length() +1;
-				char* final_format = new char[bytesToSend1];
-				strcpy(final_format, request_formatted.c_str());
-				int bytesSent1 = 0;
-			  	
-			    char* bufptr1 = final_format;
-			   
-			 	
-			    while ((bytesSent1 = send(request_fd, bufptr1, bytesToSend1, 0)) > 0){
-
-					bytesToSend1 = bytesToSend1 - bytesSent1;
-					bufptr1 += bytesSent1;
-					if (bytesToSend1 < 1){
-						break;
-					}
-					
-				}
-				
-			
-				delete[] final_format;
-
-				bufptr = recieveBuffer;
-				bufSpace = MAXDATASIZE-1;
-				while((bytesRecieved = recv(request_fd, bufptr, bufSpace, 0)) > 0 ){
-					for (int i = 0; i < bytesRecieved; i++){
-						serverResponse += bufptr[i]; 
-					}
-					memset(bufptr, 0, MAXDATASIZE-1);
-				//	if(serverResponse.length() > 3 && serverResponse.at(serverResponse.length()-1) == '\n' && serverResponse.at(serverResponse.length()-2) == '\r' && serverResponse.at(serverResponse.length()-3) == '\n' && serverResponse.at(serverResponse.length()-4) == '\r'){ 
-				//	break;
-				//	} 
-
-
-				}
-		
-			
-
-			
-		}
-		printf("%s\n",serverResponse.c_str() );
-
-		sendResponseToClient(serverResponse, new_fd);
+	int colon, colon2;
 	
-			
 
+	// add option lines to the formatted string
+	for (int i = 3; i < index; i++){
+		unformatted = lines[i];
+		colon = unformatted.find(":", 0);
+		colon2 = unformatted.find(":", colon+1);
+		// check if no colon is present in line
+		if(colon == -1 ){
+			isFormated = false;
+		}2
+		// check if there are multiple colons in a single line
+		else if(colon2 != -1 ){
+			isFormated = false;
+
+		}
+		std::string header = lines[i];
+		if (header.find("connection", 0) == std::string::npos && header.find("Connection", 0) == std::string::npos){ //we always want connection:close regardless of user input
+			request_formatted += lines[i];
+			request_formatted += "\r\n";
+		}
+	}
+	*/
+
+
+	// request check
+
+	request_formatted += "\r\n";
+	printf("%s\n",request_formatted.c_str() );
+
+	if (strcmp(lines[0], "GET") != 0){
+		serverResponse += "501 Error: Request type not supported\n";
+	}
+	// options formatting check
+	else if (!isFormated){
+		serverResponse += "500 Error: Internal Server error\n";
+	}
+	// proxy response piping
+	else{
+		int request_fd;
+		struct addrinfo hints1, *servinfo1, *p1;
+		int rv1;
+
+		memset(&hints1, 0, sizeof hints1);
+		hints1.ai_family = AF_UNSPEC;
+		hints1.ai_socktype = SOCK_STREAM;
+		hints1.ai_flags = AI_PASSIVE; // use my IP
+
+		if ((rv1 = getaddrinfo(host.c_str(), port.c_str(), &hints1, &servinfo1)) != 0) {
+			serverResponse += "500 error: Internal Server error\n";
+			sendResponseToClient(serverResponse, new_fd);
+			return NULL;
+		}
+
+		// loop through all the results and connect to the first we can
+		for(p1 = servinfo1; p1 != NULL; p1 = p1->ai_next) {
+			if ((request_fd = socket(p1->ai_family, p1->ai_socktype,
+				p1->ai_protocol)) == -1) {
+				return NULL;
+			}
+
+			if (connect(request_fd, p1->ai_addr, p1->ai_addrlen) == -1) {
+				close(request_fd);
+				return NULL;
+			}
+			break;
+		}
+
+		if (p1 == NULL) {
+			serverResponse += "500 error: Internal Server error\n";
+			sendResponseToClient(serverResponse, new_fd);
+			return NULL; // will modularize in next version
+		}
+
+		freeaddrinfo(servinfo1); // all done with this structure
+		
+		int bytesToSend1 = request_formatted.length() +1;
+		char* final_format = new char[bytesToSend1];
+		strcpy(final_format, request_formatted.c_str());
+		int bytesSent1 = 0;
+		  	
+	    char* bufptr1 = final_format;   
+		 	
+	    while ((bytesSent1 = send(request_fd, bufptr1, bytesToSend1, 0)) > 0){
+			bytesToSend1 = bytesToSend1 - bytesSent1;
+			bufptr1 += bytesSent1;
+			if (bytesToSend1 < 1){
+				break;
+			}	
+		}
 			
-          delete[] request_in;
-          return NULL;
+		delete[] final_format;
+
+		bufptr = recieveBuffer;
+		bufSpace = MAXDATASIZE-1;
+		while((bytesRecieved = recv(request_fd, bufptr, bufSpace, 0)) > 0 ){
+			for (int i = 0; i < bytesRecieved; i++){
+				serverResponse += bufptr[i]; 
+			}
+			memset(bufptr, 0, MAXDATASIZE-1);
+			//if(serverResponse.length() > 3 && serverResponse.at(serverResponse.length()-1) == '\n' && serverResponse.at(serverResponse.length()-2) == '\r' && serverResponse.at(serverResponse.length()-3) == '\n' && serverResponse.at(serverResponse.length()-4) == '\r'){ 
+			//break;
+			//} 
+		}
+	}
+	printf("%s\n",serverResponse.c_str() );
+	sendResponseToClient(serverResponse, new_fd);
+	
+  	delete[] request_in;
+  	return NULL;
 }
 
 int main(int argc, char* argv[])
@@ -369,10 +313,6 @@ int main(int argc, char* argv[])
 	std::string request_input = "";
 	int threadCount = 0;
 	std::vector<pthread_t> ThreadList(30);
-
-
-	
-
 
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
@@ -434,35 +374,26 @@ int main(int argc, char* argv[])
 		perror("sigpipe");
 		exit(1);
 	}
-
-
 	//printf("server: waiting for connections...\n");
 
 	while(1) {  // main accept() loop
 		sin_size = sizeof their_addr;
 		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-		if (new_fd == -1)
-		{
+		if (new_fd == -1){
 			//perror("accept error");
 			//exit(1);
 		}
 	
-		
-		
-		 int* arg = &new_fd;
-	 if ((unsigned) threadCount > (ThreadList.size() - 1)){
-//			ThreadList.resize(ThreadList.size()*2);
+	 	int* arg = &new_fd;
+	 	if ((unsigned) threadCount > (ThreadList.size() - 1)){
+			//ThreadList.resize(ThreadList.size()*2);
 	 		printf("resize");
 		}
-		 pthread_create(&ThreadList[0], NULL, doRequest, (void*) arg);
-		 pthread_detach(ThreadList[0]);
-//		 threadCount++;
+	 	pthread_create(&ThreadList[0], NULL, doRequest, (void*) arg);
+	 	pthread_detach(ThreadList[0]);
+		//threadCount++;
 	}
       		
-		
-		
-	
-
 	return 0;
 }
 
